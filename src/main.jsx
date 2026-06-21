a8d6997dc206a1ee15c853a5234dfc9d62256ee0
janhavisawal <53596988+janhavisawal@users.noreply.github.com>
2026-06-21 22:24:38 +0530

    Add main.jsx to render SeerDashboard component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SeerDashboard from "./SeerDashboardV2";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SeerDashboard />
+  </React.StrictMode>
+);
